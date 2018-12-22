078df3408c82acc97d2421ddf304b7093a8abb9e
beerpoldet <poladet.a@gmail.com>
2018-12-22 13:16:05 +0700

    Update signing vars

--- scripts/sign-and-upload.sh
+++ scripts/sign-and-upload.sh
@@ -20,7 +20,7 @@ echo "App: $APP_NAME"
 echo ""
 # xcrun -log -sdk iphoneos PackageApplication "$OUTPUTDIR/$APP_NAME.app" -o "$OUTPUTDIR/$APP_NAME.ipa" -sign "$DEVELOPER_NAME" -embed "$PROVISIONING_PROFILE"
 xcodebuild -project $XCODE_PROJECT -scheme $XCODE_SCHEME -sdk iphoneos -configuration AppStoreDistribution archive -archivePath $BUILDDIR/$XCODE_SCHEME.xcarchive
-xcodebuild -exportArchive -archivePath BUILDDIR/$XCODE_SCHEME.xcarchive -exportOptionsPlist exportOptions.plist -exportPath $BUILDDIR
+xcodebuild -exportArchive -archivePath $BUILDDIR/$XCODE_SCHEME.xcarchive -exportOptionsPlist exportOptions.plist -exportPath $BUILDDIR
 
 RELEASE_DATE=`date '+%Y-%m-%d %H:%M:%S'`
 RELEASE_NOTES="Build: $TRAVIS_BUILD_NUMBER\nUploaded: $RELEASE_DATE"
