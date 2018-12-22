e995f7935d435b11012a8908790ddc9fee49584c
beerpoldet <poladet.a@gmail.com>
2018-12-22 12:30:06 +0700

    Add app store connect upload

--- scripts/sign-and-upload.sh
+++ scripts/sign-and-upload.sh
@@ -11,4 +11,17 @@ fi
 PROVISIONING_PROFILE="$HOME/Library/MobileDevice/Provisioning Profiles/$PROFILE_NAME.mobileprovision"
 OUTPUTDIR="$PWD/build/Release-iphoneos"
 
+echo "***************************"
+echo "*        Signing          *"
+echo "***************************"
 xcrun -log -sdk iphoneos PackageApplication "$OUTPUTDIR/$APP_NAME.app" -o "$OUTPUTDIR/$APP_NAME.ipa" -sign "$DEVELOPER_NAME" -embed "$PROVISIONING_PROFILE"
+
+RELEASE_DATE=`date '+%Y-%m-%d %H:%M:%S'`
+RELEASE_NOTES="Build: $TRAVIS_BUILD_NUMBER\nUploaded: $RELEASE_DATE"
+
+echo ""
+echo "**********************************"
+echo "* Uploading to App Store Connect *"
+echo "**********************************"
+
+altool --upload-app -f "$OUTPUTDIR/$APP_NAME.ipa" -u $APP_STORE_CONNECT_USERNAME -p $APP_STORE_CONNECT_PASSWORD
