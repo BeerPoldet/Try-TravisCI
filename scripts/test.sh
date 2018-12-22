dd9680dd6a2f8a96897d89b7f026f9388a46bcba
beerpoldet <poladet.a@gmail.com>
2018-12-22 17:51:32 +0700

    add xcpretty-travis-formatter

--- scripts/test.sh
+++ scripts/test.sh
@@ -4,4 +4,4 @@ xcodebuild $XCODE_ROOT \
   -scheme $XCODE_SCHEME \
   -sdk iphonesimulator \
   -destination 'platform=iOS Simulator,OS=12.1,name=iPhone XS' \
-  test | xcpretty && exit ${PIPESTATUS[0]}
+  test | xcpretty -f `xcpretty-travis-formatter` && exit ${PIPESTATUS[0]}
