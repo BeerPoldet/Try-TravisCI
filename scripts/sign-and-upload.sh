f10a69e095c5e2fce5b571161382fbea11cf796b
beerpoldet <poladet.a@gmail.com>
2018-12-22 13:55:45 +0700

    Update signing vars

--- scripts/sign-and-upload.sh
+++ scripts/sign-and-upload.sh
@@ -30,5 +30,6 @@ echo "**********************************"
 echo "* Uploading to App Store Connect *"
 echo "**********************************"
 
-/Applications/Xcode.app/Contents/Applications/Application\ Loader.app/Contents/Frameworks/ITunesSoftwareService.framework/Support/altool --validate-app -f "$OUTPUTDIR/$APP_NAME.ipa" -t ios -u $APP_STORE_CONNECT_USERNAME -p $APP_STORE_CONNECT_PASSWORD
-/Applications/Xcode.app/Contents/Applications/Application\ Loader.app/Contents/Frameworks/ITunesSoftwareService.framework/Support/altool --upload-app -f "$OUTPUTDIR/$APP_NAME.ipa" -t ios -u $APP_STORE_CONNECT_USERNAME -p $APP_STORE_CONNECT_PASSWORD
+export PATH="$PATH:/Applications/Xcode.app/Contents/Applications/Application\ Loader.app/Contents/Frameworks/ITunesSoftwareService.framework/Support/"
+altool --validate-app -f "$OUTPUTDIR/$APP_NAME.ipa" -t ios -u $APP_STORE_CONNECT_USERNAME -p $APP_STORE_CONNECT_PASSWORD
+altool --upload-app -f "$OUTPUTDIR/$APP_NAME.ipa" -t ios -u $APP_STORE_CONNECT_USERNAME -p $APP_STORE_CONNECT_PASSWORD
