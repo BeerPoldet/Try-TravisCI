d64c8ac688db3cbe09b35a8eb6012f26ba6f7e91
beerpoldet <poladet.a@gmail.com>
2019-01-12 16:44:14 +0700

    https://google.com

--- scripts/prepare-git.sh
+++ scripts/prepare-git.sh
@@ -1,8 +1,3 @@
 #!/bin/bash
 
 git remote set-url origin https://${GIT_TOKEN}@github.com/${GIT_URL}.git
-
-# if ! git checkout "$TRAVIS_BRANCH"; then
-#   err "failed to checkout $TRAVIS_BRANCH"
-#   return 1
-# fi
