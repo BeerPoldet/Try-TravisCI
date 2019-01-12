497be417e8c81b39a2ba1702905dd365fc0b1f54
beerpoldet <poladet.a@gmail.com>
2019-01-12 12:29:44 +0700

    update travis branch

--- scripts/prepare-git.sh
+++ scripts/prepare-git.sh
@@ -2,7 +2,7 @@
 
 git remote set-url origin https://${GIT_TOKEN}@github.com/${GIT_URL}.git
 
-if ! git checkout "$TRAVIS_BRANCH"; then
-  err "failed to checkout $TRAVIS_BRANCH"
-  return 1
-fi
+# if ! git checkout "$TRAVIS_BRANCH"; then
+#   err "failed to checkout $TRAVIS_BRANCH"
+#   return 1
+# fi
